fa809a707628f4160cdcea4fb66528e2313108c4
Alawonde Olakunle <alawonde.olakunle@gmail.com>
2022-08-19 11:52:28 +0200

    access creation file

---
+++ log.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#this file will help onboard all members 
+#It will also help specify each members roles
+echo "Enter your name"
+read name
+echo "Enter your role"
+read role
+echo "$name, you can now push to the visa repository"
