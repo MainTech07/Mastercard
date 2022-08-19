6fbdf43262e03f7049937621389a5e6cbc40f314
Mayowa Oladipo <mayonator7@yahoo.com>
2022-08-19 12:26:31 +0100

    Allow users access to Repository

---
+++ Team.sh
@@ -0,0 +1,5 @@
+# Team name = MainTech07
+# Token = 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
+# git pull --rebase vs master
+# this command is what has been giving us the error, 
+# the command allows us to tell our github repository that another user wants to send something to the same repository allow them.
