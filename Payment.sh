834da041ada9cfd20d05c5f1f59339a9f6ee4c13
Mayowa Oladipo <mayonator7@yahoo.com>
2022-08-20 19:10:25 +0100

    Dev env Deploy

--- Payment.sh
+++ Payment.sh
@@ -1,4 +1,4 @@
-#
+# This is a new application
 echo "Please enter your name"
 read name
 echo "Please enter the fee amount you can pay for the course"
@@ -11,3 +11,4 @@ echo "You're an Engineer"
 else
 echo "Sorry $name you can't be admitted"
 fi
+# Check updates
